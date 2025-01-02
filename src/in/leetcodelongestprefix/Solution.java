938758864cccd3e0d92dd053c872a4250352a746
Gokul-Jaybhaye <jaybhayegokul3@gmail.com>
2025-01-02 12:19:11 +0530

    LeetCodeProblemNo14

---
+++ src/in/leetcodelongestprefix/Solution.java
@@ -0,0 +1,17 @@
+package in.leetcodelongestprefix;
+
+public class Solution {
+	public String longestCommonPrefix(String[] strs) {
+        int n=strs.length;
+       // String arr;
+        for(int i=0;i<strs[0].length();++i){
+            for(int j=0;j<n;++j){
+            if(strs[j].length() <= i|| strs[j].charAt(i) != strs[0].charAt(i)){
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+
+}
+}
